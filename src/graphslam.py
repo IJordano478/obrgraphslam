c07362f4b95bdb6f280de18139091e63e95dba54
Isaac Jordan <17044826@brookes.ac.uk>
2021-01-23 14:13:20 +0000

    graphslam_solve first version complete, classes for omega and xi created

--- src/graphslam.py
+++ src/graphslam.py
@@ -3,6 +3,7 @@
 GraphSLAM based on Thrun et al
 """
 from src.frame2d import *
+from src.omega import *
 
 import numpy as np
 import math
@@ -15,99 +16,165 @@ by recursively applying the velocity motion model. Since we are only interested 
 GraphSLAM_initialize only uses the deterministic part of the motion model. It also does not consider any measurement in 
 its estimation.
 '''
+timeStep = 1
+motionNoiseCovar = [1., 1., 1.]
+measureNoiseCovar = [1., 1., 1.]
 
+noiseCovarR = np.diag(motionNoiseCovar)
+noiseCovarQ = np.diag(measureNoiseCovar)
+
+landmarks = np.empty((0, 3))
+
+omega = None
+xi = None
 
 def gs_initialise(controls: np.array):
     # Set base start pose and time step. Time step should be updated in integration version to actual time delta
-    timeStep = 1
     meanX = 0
     meanY = 0
     meanTheta = 0
-
-    poses = np.empty(len(controls) + 1, dtype=Frame2D)
-    poses[0] = Frame2D().fromXYA(meanX, meanY, meanTheta)
+    poses = np.zeros((1,3))
 
     # odometry motion model. Used w==0 to control singularity for straight line driving.
     i = 1
     for [v, w] in controls:
         if w == 0:
-            meanX = meanX + (v * math.cos(meanTheta)) * timeStep
-            meanY = meanY + (v * math.sin(meanTheta)) * timeStep
+            meanX = meanX + (v * timeStep)
         else:
             meanX = meanX + ((-(v / w) * math.sin(meanTheta)) + ((v / w) * math.sin(meanTheta + w * timeStep)))
             meanY = meanY + (((v / w) * math.cos(meanTheta)) - ((v / w) * math.cos(meanTheta + w * timeStep)))
             meanTheta = meanTheta + (w * timeStep)
-        poses[i] = Frame2D().fromXYA(meanX, meanY, meanTheta)
-        i += 1
 
+        #currPose = np.array([[math.cos(meanTheta), -math.sin(meanTheta), meanX], [math.sin(meanTheta), math.cos(meanTheta), meanY], [0., 0., 1.]])
+        #poses = np.concatenate((poses,np.expand_dims(currPose, axis = 2)), axis=2)
+        currPose = np.array([[meanX, meanY, meanTheta]])
+        poses = np.concatenate((poses, currPose), axis=0)
+        i += 1
     return poses
 
 
 # 1: LINEARIZE(control, observations, correspondence, means)
 def gs_linearize(controls=None, measurements=None, poseMeans=None, correspondence=None):
     # 2: Set omega and xi to 0
-    omega = np.mat([1,1], dtype=Frame2D)
-    xi = 0
+    global omega, xi
+    omega = Omega()
+    xi = Xi()
 
     # 3: add 3x3 matrix with infinity on diagonals to omega at x0
-    infMat = np.zeros((3, 3))
-    np.fill_diagonal(infMat, np.inf)
-    omega[0, 0] = Frame2D().fromMat(infMat)
+    # Done in initialisation of Omega
 
     # 4: for all controls do
     for i in range(0,len(controls)):
         [v, w] = controls[i]
 
-        # 5: xhat = calculations
-        xhat = poseMeans[i+1]
+        # 5: xhat = the pose after the control has been applied
+        xhat = poseMeans[i+1,:]
 
         # 6: G = 3x3 matrix of calculations
-        jacobianG = Frame2D()
-        jacobianG.mat[0:2,2] = poseMeans[i+1].mat[0:2,2]
+        jacobianG = np.eye(3)
+        if w != 0:
+            #jacobianG[0,2] = (-(v / w) * math.cos(angle2num(poseMeans[0:3,0:3,i]))) + ((v / w) * math.cos(angle2num(poseMeans[0:3,0:3,i]) + w * timeStep))
+            #jacobianG[1,2] = (-(v / w) * math.cos(angle2num(poseMeans[0:3,0:3,i]))) + ((v / w) * math.cos(angle2num(poseMeans[0:3,0:3,i]) + w * timeStep))
+            jacobianG[0,2] = (-(v / w) * math.cos(poseMeans[i,2])) + ((v / w) * math.cos(poseMeans[i,2] + w * timeStep))
+            jacobianG[1,2] = (-(v / w) * math.cos(poseMeans[i,2])) + ((v / w) * math.cos(poseMeans[i,2] + w * timeStep))
+        else:
+            jacobianG[1, 2] = v*timeStep
 
         # 7: Add G and R to omega
-        motionNoiseCovar = [0.01, 0.01, 0.01]
-        R = np.diag(motionNoiseCovar)
-        R = Frame2D().fromMat(R)
-        omegaUpdate = np.vstack((np.transpose(-jacobianG.mat), [1, 1, 1])) * \
-            R.inverse().mat * np.hstack((-jacobianG.mat, np.array([[1], [1], [1]])))
-        #TODO use update
+        gt1 = np.vstack((np.transpose(-jacobianG), np.eye(3)))
+        gt2 = np.hstack((-jacobianG, np.eye(3)))
+        omegaUpdate = np.matmul(np.matmul(gt1, np.linalg.inv(noiseCovarR)), gt2)
+        omega.addPose(omegaUpdate, i+1)
+        print("Omega:\n",omegaUpdate)
 
         # 8: add same with a bit more to xi
-        xiUpdate = np.vstack((np.transpose(-jacobianG.mat), [1, 1, 1])) * R.inverse().mat * np.hstack((xhat-jacobianG, poseMeans[i]))
-        #TODO use update
+        #gt3 = np.hstack((xhat-jacobianG * poseMeans[0:3,0:3,i]))
+        gt3 = xhat - np.matmul(jacobianG, poseMeans[i])
+        xiUpdate = np.matmul(np.matmul(gt1, np.linalg.inv(noiseCovarR)), gt3)
+        xi.addPose(xiUpdate, i+1)
+        print("Xi:\n", xiUpdate)
 
     # 9: endfor
 
     # 10: for all measurements zt do
-    for i in range(0, len(measurements)):
+    for i in range(0, measurements.shape[0]):
+        measurement = measurements[i]
+        time = int(measurement[0])
+        print("Time:",time)
         # 11: Qt = sigma squared for r, phi and s
-        measureNoiseCovar = [0.01, 0.01, 0.01]
-        Q = np.diag(measureNoiseCovar)
-        Q = Frame2D().fromMat(Q)
+        # Skipped as same sensor used, so noise declared globally
 
-        # 12: for all observed features
+        # 12: for all observed features at time
+        #for m in measurements:
 
-            # 13: j = observed landmark (c.i.t)
+        # 13: j = observed landmark (c.i.t)
+        # TODO j needs to be the actual landmark position that was noted, this is a placeholder that make ones from the
+        #  measurement. j: [x,y,signature]
+        pose = poseMeans[int(measurement[0]),:]
+        pose2d = np.array([[1, 0, pose[0]],
+                             [0, 1, pose[1]],
+                             [0, 0, 1]])
+        pose2d[0:2, 0:2] = num2matangle(pose[2])
 
-            # 14: delta = [[deltax],[deltay]]
+        relative = np.array([[1, 0, math.cos(measurement[2])*measurement[1]],
+                             [0, 1, math.sin(measurement[2])*measurement[1]],
+                             [0, 0, 1]])
+        relative[0:2,0:2] = num2matangle(measurement[2])
+        lmark = np.matmul(pose2d, relative)
+        j = lmark[0:3,2]
+        j[2] = 0
 
-            # 15: q = transpose(delta*delta)
+        # TODO this part to next todo is poor, landmarks should be recorded during data collection
+        match = False
+        global landmarks
+        for n in range(0,landmarks.shape[0]):
+            if ((landmarks[n,0]-j[0])*(landmarks[n,0]-j[0]) + (landmarks[n,1]-j[1])*(landmarks[n,1]-j[1]) < (0.5)):
+                j = landmarks[n,:]
+                match = True
+                index = n
+                print("matched measurement to prexisting landmark")
+                break
 
-            # 16: zhat = (vector of stuff)
+        if (match == False):
+            print("no match from measurement, adding to landmarks")
+            index = landmarks.shape[0] + 1
+            if(landmarks.shape[0]==0):
+                landmarks = np.expand_dims((j), axis=0)
+            else:
+                landmarks = np.concatenate((landmarks, np.expand_dims((j), axis=0)), axis=0)
+        # TODO
 
-            # 17: H.i.t = Jacobian
+        # 14: delta = [[deltax],[deltay]]
+        delta = np.array([j[0]-pose[0], j[1]-pose[1]])
 
-            # 18: add H.i.t. and Qt^-1 to omega at xt and mj
+        # 15: q = transpose(delta*delta)
+        q = np.matmul(np.transpose(delta),delta)
 
-            # 19: add lots of stuff to xi
+        # 16: zhat = (vector of stuff)
+        zhat = np.array([math.sqrt(q), (math.atan2(delta[1],delta[0])-pose[2]), 0])
 
+        # 17: H.i.t = Jacobian
+        jacobianH = (1/q)*np.array([[-math.sqrt(q)*delta[0], -math.sqrt(q)*delta[1], 0, math.sqrt(q)*delta[0], math.sqrt(q)*delta[1], 0],
+                                    [delta[1], -delta[0], -q, -delta[1], delta[0], 0],
+                                    [0, 0, 0, 0, 0, q]])
+
+        # 18: add H.i.t. and Qt^-1 to omega at xt and mj
+        np.linalg.inv(noiseCovarQ)
+        omegaUpdate = np.matmul(np.transpose(jacobianH), np.linalg.inv(noiseCovarQ))
+        omegaUpdate = np.matmul(omegaUpdate, jacobianH)
+        omega.addLandmark(omegaUpdate, time, index)
+
+        # 19: add lots of stuff to xi
+        ht2 = measurement[1:]-zhat + np.matmul(jacobianH, np.array([pose[0], pose[1], pose[2], j[0], j[1], 0]))
+        xiUpdate = np.matmul(np.transpose(jacobianH), np.linalg.inv(noiseCovarQ))
+        xiUpdate = np.matmul(xiUpdate, ht2)
+        xi.addLandmark(xiUpdate, time, index)
         # 20: endfor
 
     # 21: endfor
 
     # 22: return omega, xi
-    return
+    return omega, xi
 
 
 # 1: REDUCE(omega, xi)
@@ -153,9 +220,12 @@ def gs_solve():
     return
 
 
-# 1: TEST(omega, xi, mean, )
-def gs_known_correspondence_test():
+# 1: TEST(omega, xi, mean, landmark1, landmark2)
+def gs_known_correspondence_test(omega, xi, poseMean, pathCovariance, lmarkJ, lmarkK):
     # 2-7: A butt tonne of maths
+
+
+
     return
 
 
@@ -163,15 +233,38 @@ def GraphSLAM():
     return
 
 
+def angle2num(matAngle):
+    return math.atan2(matAngle[1, 0], matAngle[0, 0])
+
+def num2matangle(angle):
+    return np.array([[math.cos(angle), -math.sin(angle)],
+                     [math.sin(angle), math.cos(angle)]])
+
 if __name__ == "__main__":
     # controls = np.array([[1,0],[1,0],[0,math.pi/2],[1,0],[1,0],[0,math.pi/2],[1,0]])
     # controls = np.array([[2, math.pi/2], [2, math.pi/2], [2, math.pi/2], [2, math.pi/2]])
-    controls = np.array(
-        [[1, 0], [1, 0], [2, math.pi / 2], [2, math.pi / 2], [2, math.pi / 2], [2, math.pi / 2], [0, math.pi], [2, 0],
-         [0, math.pi]])
-    measurements = None
 
+    pose1 = np.array([[1, 0, 1], [0, 1, 0], [0, 0, 1]])
+    pose2 = np.array([[1, 0, 2], [0, 1, 0], [0, 0, 1]])
+    pose3 = np.array([[0, -1, 3], [1, 0, 1], [0, 0, 1]])
+    measurements = np.array([[1, math.sqrt(2), math.pi/4, 0],
+                             [1, math.sqrt(5), -0.463647, 0],
+                             [2, math.sqrt(2), -math.pi/4, 0],
+                             [3, 1, 0, 0]])
+    controls = np.array([[1, 0], [1, 0], [math.pi / 2, math.pi / 2]])
+
+    #omega.addPose(pose1)
+    #omega.addPose(pose2)
+
+    #controls = np.array(
+    #    [[1, 0], [1, 0], [2, math.pi / 2], [2, math.pi / 2], [2, math.pi / 2], [2, math.pi / 2], [0, math.pi], [2, 0],
+    #     [0, math.pi]])
+
+    #measurements = None
     meanPoses = gs_initialise(controls)
-    for pos in meanPoses:
-        print(pos)
+
+    print("Recorded poses:")
+    for i in range(0, meanPoses.shape[0]):
+        print(np.round(meanPoses[i,:], 3))
     gs_linearize(controls,measurements, meanPoses)
+    omega.showOmega()
---
+++ src/omega.py
@@ -0,0 +1,150 @@
+import numpy as np
+import math
+import logging
+
+class Omega:
+    def __init__(self):
+        self.nPoses = 0
+        self.nLmarks = 0
+        self.poses = np.empty((3, 3, 0))                #3x3 x depth
+        self.landmarks = np.empty((3, 3, 0))            #3x3 x depth
+        self.omegaMatrix = np.empty((1, 1, 3, 3))       #row x col x transmatrix
+
+        self.poses = np.concatenate((self.poses, np.expand_dims(np.identity(3), axis=2)), axis=2)
+
+        a = np.zeros((3, 3))
+        np.fill_diagonal(a,np.inf)
+        self.omegaMatrix[0,0] = a
+
+    def addPose(self, newPose, time):
+        #If a pose is already recorded for this time
+        if(time <= self.nPoses):
+            print("Something has gone wrong here")
+
+        # else the time is beyond currently stored poses, a resize is needed
+        else:
+
+            xx = self.omegaMatrix[0:self.nPoses + 1, 0:self.nPoses + 1, :, :]
+
+            # if landmarks exist, adapt size of xx, xm and mx by adding 1 to the relative dimension filled with
+            # 3x3 matices containing zeros
+            if(self.nLmarks > 0):
+                newCol = np.zeros((self.nPoses + 1, 1, 3, 3))
+                newRow = np.zeros((1, self.nPoses + 2, 3, 3))
+                xx = np.concatenate((xx, newCol), axis=1)
+                xx = np.concatenate((xx, newRow), axis=0)
+
+                newRow = np.zeros((1, self.nLmarks, 3, 3))
+                xm = self.omegaMatrix[0:self.nPoses + 1, self.nPoses + self.nLmarks + 1, :, :]
+                xm = np.concatenate((xm, newRow), axis=0)
+
+                newCol = np.zeros((self.nLmarks, 1, 3, 3))
+                mx = self.omegaMatrix[self.nPoses+self.nLmarks+1, 0:self.nPoses+1, :, :]
+                mx = np.concatenate((mx, newCol), axis=1)
+
+                mm = self.omegaMatrix[self.nPoses+self.nLmarks+1, self.nPoses+self.nLmarks+1, :, :]
+
+                topOmega = np.concatenate((xx, xm), axis=1)
+                bottomOmega = np.concatenate((mx, mm), axis=1)
+
+                self.omegaMatrix = np.concatenate((topOmega, bottomOmega), axis=0)
+
+            # No landmarks currently stored, just need to adapt the xx portion of the matrix
+            else:
+                newCol = np.zeros((self.nPoses+1,1,3,3))
+                newRow = np.zeros((1, self.nPoses + 2, 3, 3))
+                xx = np.concatenate((xx, newCol), axis=1)
+                xx = np.concatenate((xx, newRow), axis=0)
+                self.omegaMatrix = xx
+
+            #Use update to adjust relevant entries
+            self.omegaMatrix[time-1, time-1, :, :] += newPose[0:3, 0:3]
+            self.omegaMatrix[time-1, time, :, : ] += newPose[0:3, 3:6]
+            self.omegaMatrix[time, time-1, :, :] += newPose[3:6, 0:3]
+            self.omegaMatrix[time, time, :, :] += newPose[3:6, 3:6]
+
+            print("Omega Matrix shape:", self.omegaMatrix.shape)
+
+        self.nPoses += 1
+        return
+
+    def addLandmark(self, newLandmark, time, lmarkIndex):
+        if (lmarkIndex > self.nLmarks):
+            #corresponds to new landmark
+            newCol = np.zeros((self.nPoses + self.nLmarks + 1, 1, 3, 3))
+            newRow = np.zeros((1, self.nPoses + self.nLmarks + 2, 3, 3))
+            self.omegaMatrix = np.concatenate((self.omegaMatrix, newCol), axis=1)
+            self.omegaMatrix = np.concatenate((self.omegaMatrix, newRow), axis=0)
+
+            self.omegaMatrix[time, time, :, :] += newLandmark[0:3, 0:3]
+            self.omegaMatrix[time, -1, :, :] += newLandmark[0:3, 3:6]
+            self.omegaMatrix[-1, time, :, :] += newLandmark[3:6, 0:3]
+            self.omegaMatrix[-1, -1, :, :] += newLandmark[3:6, 3:6]
+            print("Omega Matrix shape:", self.omegaMatrix.shape)
+
+            self.nLmarks += 1
+        else:
+            #corresponds to prexisting landmark
+            self.omegaMatrix[time, time, :, :] += newLandmark[0:3, 0:3]
+            self.omegaMatrix[time, self.nPoses+lmarkIndex, :, :] += newLandmark[0:3, 3:6]
+            self.omegaMatrix[self.nPoses+lmarkIndex, time, :, :] += newLandmark[3:6, 0:3]
+            self.omegaMatrix[self.nPoses+lmarkIndex, self.nPoses+lmarkIndex, :, :] += newLandmark[3:6, 3:6]
+
+        return
+
+    def updatePose(self, pos, omegaUpdate):
+        self.omegaMatrix[pos[0]][pos[1]][:][:] = omegaUpdate
+        self.omegaMatrix[pos[1]][pos[2]][:][:] = omegaUpdate
+        return
+
+    def showOmega(self):
+        mat = np.zeros((self.nPoses + self.nLmarks + 1, self.nPoses + self.nLmarks + 1), dtype=int)
+        for x in range(0, self.nPoses + self.nLmarks+1):
+            for y in range(0, self.nPoses + self.nLmarks+1):
+                if np.allclose(self.omegaMatrix[x,y,:,:], np.zeros(3)):
+                    mat[x,y] = 0
+                else:
+                    mat[x,y] = 1
+
+        print(mat)
+        return
+
+
+class Xi:
+    def __init__(self):
+        self.nPoses = 0
+        self.nLmarks = 0
+        self.xiVector = np.zeros((1, 3)) #matrix where each row is an x,y,theta (poses) or x,y,signature (lmarks)
+
+    def addPose(self, pos, time):
+        if(time <= self.nPoses):
+            print("Something has gone horribly wrong")
+        else:
+            self.xiVector = np.concatenate((self.xiVector, np.zeros((1,3))), axis=0)
+            self.xiVector[time-1,:] += pos[0:3]
+            self.xiVector[time,:] += pos[3:6]
+            self.nPoses+=1
+        return
+
+    def addLandmark(self, update, time, lmarkIndex):
+        if(lmarkIndex > self.nLmarks):
+            # Corresponds to new landmark
+            self.xiVector = np.concatenate((self.xiVector, np.zeros((1, 3))), axis=0)
+            self.xiVector[time, :] += update[0:3]
+            self.xiVector[self.nPoses + lmarkIndex,:] += update[3:6]
+            self.nLmarks += 1
+
+            print("xi shape", self.xiVector.shape)
+            print(self.xiVector)
+        else:
+            # Corresponds to prexisting landmark
+            self.xiVector[time, :] += update[0:3]
+            self.xiVector[self.nPoses+lmarkIndex, :] += update[3:6]
+            print("xi shape", self.xiVector.shape)
+            print(self.xiVector)
+
+
+
+
+
+        return
